497bc316a21522afb2fd1780cb5aa1650de8c115
emoceann <shexaergashev@gmail.com>
2023-04-24 05:42:55 +0500

    fix

--- src/bot/api.py
+++ src/bot/api.py
@@ -1,7 +1,6 @@
 from src.bot.handlers import dp, bot
 from src.bot.deps import app
 from aiogram import Bot, Dispatcher, types
-from src.exchangeratesapi.services import get_dict_currencies
 
 
 @app.post("/internal")
@@ -10,7 +9,3 @@ async def webhook(update: dict):
     Dispatcher.set_current(value=dp)
     await dp.process_update(update=types.Update(**update))
 
-
-@app.get('/rwre')
-async def sdfsd():
-    return await get_dict_currencies()
